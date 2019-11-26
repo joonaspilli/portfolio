04c3f004c49a3c6db3f7f97f4ce5a4d5694702ad
Joonas Pilli <joonas_pilli@hotmail.com>
2019-11-26 19:06:28 +0200

    Switch to use a CSS-based solution for smooth-scrolling

--- scripts.js
+++ scripts.js
@@ -3,50 +3,11 @@ window.addEventListener('DOMContentLoaded', function() {
   const GLOBALS = {
     NAVIGATION: {
       HIDE_CSS_CLASS: 'vhidden'
-    },
-    ANCHOR_LINKS: {
-      SCROLL_Y_OFFSET: 96
     }
   };
 
   const asArray = function(e) {
     return Array.prototype.slice.call(e);
-  };
-
-  const onAnchorLinkClick = function(event, link) {
-    const href = link.getAttribute('href');
-    event.preventDefault();
-    window.history.pushState(null, '', href);
-    // setTimeout to allow navigation to hide before scrolling.
-    window.setTimeout(function() {
-      const anchoredEl = document.querySelector(href);
-      const anchoredElTabIndex = anchoredEl.getAttribute('tabindex');
-      window.scrollTo({
-        top: anchoredEl.getBoundingClientRect().y + window.scrollY
-          - GLOBALS.ANCHOR_LINKS.SCROLL_Y_OFFSET,
-        left: anchoredEl.getBoundingClientRect().x + window.scrollX,
-        behavior: 'smooth'
-      });
-      if (!anchoredElTabIndex) {
-        anchoredEl.setAttribute('tabindex', '-1');
-      }
-      anchoredEl.focus({ preventScroll: true });
-      if (!anchoredElTabIndex) {
-        anchoredEl.removeAttribute('tabindex');
-      }
-    });
-  };
-
-  const initializeSmoothAnchorLinks = function() {
-    asArray(document.querySelectorAll('a[href^="#"]'))
-      .filter(function(link) {
-        return link.getAttribute('href').length > 1;
-      })
-      .forEach(function(link) {
-        link.addEventListener('click', function(event) {
-          onAnchorLinkClick(event, link);
-        });
-      });
   };
 
   const displayNavigation = function(nav) {
@@ -104,7 +65,7 @@ window.addEventListener('DOMContentLoaded', function() {
       .forEach(initNavigationButtons);
   };
 
-  initializeSmoothAnchorLinks();
   initializeNavigations();
+
 });
 
